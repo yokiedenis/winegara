b2f219f7b9bb22fc87bbd95a5ef9aa461079d402
unknown <yokasdenis@gmail.com>
2025-01-19 08:38:57 +0300

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ import ShoppingCheckout from "./pages/shopping-view/checkout";
 import ShoppingAccount from "./pages/shopping-view/account";
 import CheckAuth from "./components/common/check-auth";
 import UnauthPage from "./pages/unauth-page";
-import {useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { checkAuth } from "./store/auth-slice";
 import { useEffect } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -32,24 +32,24 @@ function App() {
     dispatch(checkAuth());
   }, [dispatch]);
 
-  if(isLoading) return <Skeleton className="w-[800px] bg-black h-[600px]" />
+  if (isLoading) return <Skeleton className="w-[800px] bg-black h-[600px]" />
 
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <Routes>
         <Route path="/auth" element={
           <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-          <AuthLayout />
-        </CheckAuth>
-      }>
+            <AuthLayout />
+          </CheckAuth>
+        }>
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
         <Route path="/admin" element={
           <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-         <AdminLayout />
-        </CheckAuth>
-      }>
+            <AdminLayout />
+          </CheckAuth>
+        }>
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
           <Route path="orders" element={<AdminOrders />} />
@@ -58,16 +58,16 @@ function App() {
         </Route>
         <Route path="/shop" element={
           <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-          <ShoppingLayout />
-        </CheckAuth>
-      }>
+            <ShoppingLayout />
+          </CheckAuth>
+        }>
           <Route path="home" element={<ShoppingHome />} />
           <Route path="listing" element={<ShoppingListing />} />
           <Route path="checkout" element={<ShoppingCheckout />} />
           <Route path="account" element={<ShoppingAccount />} />
           <Route path="paypal-return" element={<PaypalReturnPage />} />
-                    <Route path="payment-success" element={<PaymentSuccessPage />} />
-                    <Route path="search" element={<SearchProducts />} />
+          <Route path="payment-success" element={<PaymentSuccessPage />} />
+          <Route path="search" element={<SearchProducts />} />
         </Route>
         <Route path="*" element={<NotFound />} />
         <Route path="/unauth-page" element={<UnauthPage />} />
