2148b3832b77e4730d8bda0c7d14858d0e82b45a
unknown <yokasdenis@gmail.com>
2025-01-19 12:33:10 +0300

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
 
   if (isLoading) return <Skeleton className="w-[800] bg-black h-[600px]" />;
 
-  console.log(isLoading, user);
+  // console.log(isLoading, user);
 
   return (
     <div className="flex flex-col overflow-hidden bg-white">
