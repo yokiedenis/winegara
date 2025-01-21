d2c310e3a0b22d171d19df253c3e5b47c054a501
unknown <yokasdenis@gmail.com>
2025-01-21 04:52:26 +0300

    first commit

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -115,7 +115,7 @@ function HeaderRightContent({ closeSheet }) {
               ? cartItems.items
               : []
           }
-          closeSheet={closeSheet}
+          
         />
       </Sheet>
 
