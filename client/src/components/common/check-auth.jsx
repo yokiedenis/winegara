4d37d45ac1de307d49d7cfaaf0ee6110e4163fcd
unknown <yokasdenis@gmail.com>
2025-02-06 04:19:22 +0300

    first commit

--- client/src/components/common/check-auth.jsx
+++ client/src/components/common/check-auth.jsx
@@ -2,22 +2,14 @@ import { Navigate, useLocation } from "react-router-dom";
 
 function CheckAuth({ isAuthenticated, user, children }) {
     const location = useLocation();
-    if (!isAuthenticated && location.pathname === "/shop/home") {
-        return <>{children}</>;
-    }
-    if (!isAuthenticated && location.pathname === "/shop/listing") {
-        return <>{children}</>;
-    }
-    if (!isAuthenticated && location.pathname === "/shop/search") {
-        return <>{children}</>;
-    }
+   
     
 
     if (!isAuthenticated && !["/login", "/register"].some(path => location.pathname.includes(path))) {
         return <Navigate to="/auth/login" />;
     }
 
-if(isAuthenticated && (location.pathname.includes("/login")||location.pathname.includes("/register"))){
+if(isAuthenticated && (location.pathname.includes("/login")||location.pathname.includes("/register") || location.pathname.includes("/shop/home")    )){
     if(user?.role==="admin"){
 return <Navigate to ="/admin/dashboard"/>
     }else{
@@ -27,7 +19,7 @@ return <Navigate to ="/admin/dashboard"/>
 if(isAuthenticated && user?.role!=="admin" && location.pathname.includes("admin")){
     return <Navigate to="/unauth-page"/>
 }
-if(isAuthenticated && user?.role==="admin" && location.pathname.includes("shop")){
+if(isAuthenticated && user?.role==="user" && location.pathname.includes("shop")){
     return <Navigate to ="/admin/dashboard"/>
 }
 return <>{children}</>
