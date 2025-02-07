b2e06b40e95391404784fbe672c1dd91837275e9
unknown <yokasdenis@gmail.com>
2025-02-07 22:14:09 +0300

    first commit

--- server/controllers/shop/cart-controller.js
+++ server/controllers/shop/cart-controller.js
@@ -28,7 +28,8 @@ app.use(
   })
 );
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI,{ useNewUrlParser: true,useUnifiedTopology: true,
+    useCreateIndex: true })
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
--- server/index.js
+++ server/index.js
@@ -51,7 +51,8 @@ app.use(
 
 //create a database connection 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI,{ useNewUrlParser: true,useUnifiedTopology: true,
+    useCreateIndex: true })
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
